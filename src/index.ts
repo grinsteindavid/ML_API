5eb7324c329356193853160b9b39519c0ff53450
david <davidmirandagrinstein@gmail.com>
2021-09-27 04:06:54 -0300

    listEndpointslistEndpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import listEndpoints from 'express-list-endpoints';
 import app from './app';
 
 app.listen(3000, () => {
+    console.log(listEndpoints(app));
     console.log(`HTTP Server port ${3000}`);
 });
