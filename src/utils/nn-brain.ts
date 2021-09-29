3d891c6c66997d7599f216c71d89f4713e557c42
david <davidmirandagrinstein@gmail.com>
2021-09-29 04:54:15 -0300

    refactor

--- src/utils/nn-brain.ts
+++ src/utils/nn-brain.ts
@@ -4,5 +4,5 @@ import { NNInput } from '../types/nn-input';
 export const predictTraffic = (net: NeuralNetwork, input: NNInput): number => {
     const result = net.run<NNInput, number[]>(input);
 
-    return result ? result[0] : 0;
+    return result[0];
 };
